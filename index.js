b531bbcfedb2c5a02aa4c583e56991a951e67da1
makinej37 <juuso.s.makinen@aalto.fi>
2024-08-13 19:36:46 +0300

    lint

--- index.js
+++ index.js
@@ -9,110 +9,110 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 let persons = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
+  {
+    'id': '1',
+    'name': 'Arto Hellas',
+    'number': '040-123456'
+  },
+  {
+    'id': '2',
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523'
+  },
+  {
+    'id': '3',
+    'name': 'Dan Abramov',
+    'number': '12-43-234345'
+  },
+  {
+    'id': '4',
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122'
+  }
 ]
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
+
+  next(error)
+}
 
 const Contact = require('./models/contact')
 
 app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(contacts => {
-        response.json(contacts)
-    })
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Contact.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-    Contact.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-
-app.get("/info", (request, response) => {
-    const now = new Date;
-
-    response.send(`Phonebook has info for ${persons.length} people\n ${now}`)
-})
-
-app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'Name missing'
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'Number missing'
-        })
-    }
-
-    const person = new Contact({
-        number: body.number,
-        name: body.name,
-    })
-
-    person.save().then(savedcontact => {
-        response.json(savedcontact)
+  Contact.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then( () => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+app.get('/info', (request, response) => {
+  const now = new Date
+
+  response.send(`Phonebook has info for ${persons.length} people\n ${now}`)
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name missing'
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'Number missing'
+    })
+  }
+
+  const person = new Contact({
+    number: body.number,
+    name: body.name,
+  })
+
+  person.save().then(savedcontact => {
+    response.json(savedcontact)
+  })
+    .catch(error => next(error))
+})
+
 app.put('/api/persons/:id', (request, response, next) => {
-    const person = {
-        name: request.body.name,
-        number: request.body.number,
-    }
-    Contact.findByIdAndUpdate(request.params.id, person)
-        .then(updated => {
-            response.json(updated)
-        })
-        .catch(error => next(error))
-        
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+  }
+  Contact.findByIdAndUpdate(request.params.id, person)
+    .then(updated => {
+      response.json(updated)
+    })
+    .catch(error => next(error))
+
 })
 
 app.use(errorHandler)
--- models/contact.js
+++ models/contact.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 const phoneNumberValidator = [
   {
     validator: function(v) {
-      return /^\d{2,3}-\d{5,}$/.test(v);
+      return /^\d{2,3}-\d{5,}$/.test(v)
     },
     message: props => `${props.value} is not a valid phone number!`
   }
@@ -16,7 +16,7 @@ const phoneNumberValidator = [
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
+  .then( () => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
